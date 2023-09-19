0d6ec286ecc0ac64a7ba8e78d87ac2c88d643e83
EconMaett <matthias.spichiger@bluewin.ch>
2023-09-19 15:18:15 +0200

    Initial commit

---
+++ R/cleaning-helpers.R
@@ -0,0 +1,37 @@
+# cleaning-helpers.R
+english <- temp <- NULL
+
+lookup_table <- dplyr::tribble(
+  ~where, ~english,
+  "beach",     "US",
+  "coast",     "US",
+  "seashore",     "UK",
+  "seaside",     "UK"
+)
+
+#' @export
+localize_beach <- function(dat) {
+  dplyr::left_join(dat, lookup_table)
+}
+
+f_to_c <- function(x) (x - 32) * 5/9
+
+#' @export
+celsify_temp <- function(dat) {
+  dplyr::mutate(dat, temp = dplyr::if_else(english == "US", f_to_c(temp), temp))
+}
+
+timestamp <- function(time = Sys.time()) {
+  withr::with_locale(
+    new = c("LC_TIME" = "C"),
+    code = format(time, "%Y-%B-%d_%H-%M-%S", tz = "UTC")
+  )
+}
+
+#' @export
+outfile_path <- function(infile, time = Sys.time()) {
+  ts <- timestamp(time)
+  paste0(ts, "_", sub("(.*)([.]csv$)", "\\1_clean\\2", infile))
+}
+
+# END
